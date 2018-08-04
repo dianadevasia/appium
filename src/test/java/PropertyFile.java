b5d92ad2a80147c7884efba16185560820c7e370
Diana Devasia <dianadev@thoughtworks.com>
2018-08-04 22:43:39 +0200

    Added yaml

---
+++ src/test/java/PropertyFile.java
@@ -0,0 +1,29 @@
+public class PropertyFile {
+    String clearApkDataOnStart;
+    String searchKeywords0;
+    String productTitle0;
+
+    public String getClearApkDataOnStart() {
+        return clearApkDataOnStart;
+    }
+
+    public void setClearApkDataOnStart( final String clearApkDataOnStart ) {
+        this.clearApkDataOnStart = clearApkDataOnStart;
+    }
+
+    public String getSearchKeywords0() {
+        return searchKeywords0;
+    }
+
+    public void setSearchKeywords0( final String searchKeywords0 ) {
+        this.searchKeywords0 = searchKeywords0;
+    }
+
+    public String getProductTitle0() {
+        return productTitle0;
+    }
+
+    public void setProductTitle0( final String productTitle0 ) {
+        this.productTitle0 = productTitle0;
+    }
+}
--- src/test/java/appiumTest.java
+++ src/test/java/appiumTest.java
@@ -1,9 +1,7 @@
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -16,6 +14,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
@@ -32,107 +33,130 @@ public class appiumTest {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability("deviceName", "ZY2239T773");
-        capabilities.setCapability(CapabilityType.BROWSER_NAME, "Android");
-        capabilities.setCapability(CapabilityType.VERSION, "7.0.1");
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("appPackage", "com.amazon.mShop.android.shopping");
-        capabilities.setCapability("appActivity", "com.amazon.mShop.home.HomeActivity");
-        capabilities.setCapability( MobileCapabilityType.FULL_RESET, false);
-        capabilities.setCapability( MobileCapabilityType.NO_RESET, true);
+        capabilities.setCapability( "deviceName", "ZY2239T773" );
+        capabilities.setCapability( CapabilityType.BROWSER_NAME, "Android" );
+        capabilities.setCapability( CapabilityType.VERSION, "7.0.1" );
+        capabilities.setCapability( "platformName", "Android" );
+        capabilities.setCapability( "appPackage", "com.amazon.mShop.android.shopping" );
+        capabilities.setCapability( "appActivity", "com.amazon.mShop.home.HomeActivity" );
+        capabilities.setCapability( MobileCapabilityType.FULL_RESET, false );
+        capabilities.setCapability( MobileCapabilityType.NO_RESET, true );
 
-//        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities) {};
-        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-        driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+        //        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities) {};
+        driver = new AndroidDriver<>( new URL( "http://127.0.0.1:4723/wd/hub" ), capabilities );
+        driver.manage().timeouts().implicitlyWait( 25, TimeUnit.SECONDS );
     }
-
 
     @Test
     public void checkoutProductFlow() {
-        Properties prop = new Properties();
-        FileInputStream fileInput = null;
-        try {
-            fileInput = new FileInputStream(new File("datafile.properties"));
-            prop.load(fileInput);
-        } catch ( IOException e ) {
-            System.out.print( "file not found" );
-            e.printStackTrace();
-        }
-
-        if(prop.getProperty( "clear_apk_data_on_start" ).equals( "true" ))
-            driver.resetApp();
-
-        //        WebElement loginAsButton = driver.findElement( By.xpath( "//android.widget.Button[@text='Continue']" ) );
-        //        loginAsButton.click();
-
-        final String search_keywords_0 = prop.getProperty( "search_keywords_0" );
-        WebElement searchTextView = driver.findElement( By.xpath( "//android.widget.EditText" ) );
-        searchTextView.sendKeys( search_keywords_0 + " \n" );
-
-        driver.findElement( By.id( "com.amazon.mShop.android.shopping:id/rs_results_image" ) ).click();
-
-        final WebElement closeButton = driver.findElement( By.id("tutorial_tool_tip_close_button") );
-        closeButton.click();
-//
-        final String product_title_0 = prop.getProperty( "product_title_0" );
-//        while ( !isElementPresent() ){
-        waitForElementToBePresent(product_title_0);
+//        Properties prop = new Properties();
+//        FileInputStream fileInput = null;
+//        try {
+//            fileInput = new FileInputStream( "datafile.properties" );
+//            prop.load( fileInput );
+//        } catch ( IOException e ) {
+//            System.out.print( "file not found" );
+//            e.printStackTrace();
 //        }
 
-        final WebElement addToCart1 = driver.findElement( By.id("add-to-cart-button") );
-        addToCart1.click();
+        PropertyFile prop;
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        try {
+            prop = mapper.readValue( new File( "datafile.yaml" ), PropertyFile.class );
+            final String product_title_0 = prop.getProductTitle0();
+            if ( prop.getClearApkDataOnStart().equals( "true" ) )
+                driver.resetApp();
+            final String search_keywords_0 = prop.getSearchKeywords0();
+            WebElement searchTextView = driver.findElement( By.xpath( "//android.widget.EditText" ) );
+            searchTextView.sendKeys( search_keywords_0 + "  \n" );
 
-//        final WebElement addToCart = driver.findElement(By.xpath( "//android.widget.Button[@text='Add to Basket']" ) );
-//        JavascriptExecutor js = (JavascriptExecutor) driver;
-//        js.executeScript( "arguments[0].scrollIntoView(true);", addToCart );
 
+            waitForElementToBePresentByTextAndThenClick( product_title_0 );
+
+            //        driver.findElement( By.id( "com.amazon.mShop.android.shopping:id/rs_results_image" ) ).click();
+            waitForElementToBePresentByIdAndThenClick( "add-to-cart-button" );
+
+        } catch ( NoSuchElementException| IOException e ) {
+            System.out.println( "Element not found for tooltip or edit text to enter keyword" );
+            e.printStackTrace();
+        }
     }
 
-    private void waitForElementToBePresent(String productTitle) {
+    private void waitForElementToBePresentByIdAndThenClick( String id) {
         try {
-            boolean a = isElementPresent( productTitle );
+            while (!isElementPresentById( id )){
+                swipeUpElement( 700, 500);
+            }
+            final WebElement webElement =
+//                    (WebElement) driver.findElements(By.id( id )).get( 0 );
+//                    driver.findElement(By.xpath("//android.widget.Button[@text='Add to Basket']"));
+                    driver.findElement( By.xpath("//android.widget.Button[contains(@resource-id,'add-to-cart-button') and @text='Add to Basket']"));
+
+            swipeUpElement( 700, 530 );
+            Thread.sleep( 5000 );
+            webElement.click();
+            Thread.sleep( 10000 );
             System.out.print( "element found -- inside waitforElement" );
-//            scrollWithTouchAction("up", 0, 0, 280, 1700, 1);
-        } catch (NoSuchElementException ex) {
-            System.out.println( "element not found -- inside catch waitForElement" );
-            scrollWithTouchAction("up", 0, 0, 280, 1700, 1);
+        } catch ( NoSuchElementException ex ) {
+            swipeUpElement( 700, 500 );
+        } catch ( InterruptedException e ) {
+            e.printStackTrace();
         }
     }
 
-    public void scrollWithTouchAction(String direction, int x1, int x2, int y1, int y2, int time) {
-        TouchAction touchAction = new TouchAction(driver);
-
-        for (int i = 0; i < time; i++) {
-            if (direction.equals("down")) {
-                touchAction.press(x1, y1).moveTo(0, -y2).perform();
-            } else if (direction.equals("up")) {
-                touchAction.press(x2, y2).moveTo(x1, y1).release().perform();
-            }
-            try {
-                Thread.sleep(200); //sleep between scroll
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-
-    private boolean isElementPresent( String id ) {
+    private void waitForElementToBePresentByTextAndThenClick( String text ) {
         try {
-//            return driver.findElements( By.id( "//add-to-cart-button" ) ).size() > 0;
-            driver.findElement(By.linkText( id ));
-            scrollWithTouchAction("up", 0, 0, 280, 1500, 1);
-            scrollWithTouchAction("up", 0, 0, 280, 1500, 1);
-//            scrollWithTouchAction("up", 0, 0, 280, 1500, 1);
-            final WebElement element = driver.findElement(By.linkText( id ));
-            element.click();
-            return true;
-//            return driver.findElements(By.xpath( "//android.widget.Button[@text='Add to Basket']" ) ).size() > 0;
-        }catch ( NoSuchElementException ex) {
-            System.out.print( "element not found -- inside catch" );
+            while (!isElementPresentByText( text )){
+                swipeUpElement( 700, 600);
+            }
+            final WebElement webElement =
+                    (WebElement) driver.findElements(By.xpath("//*[contains(@text,'"+text+"')]")).get( 0 );
+            webElement.click();
+        } catch ( NoSuchElementException ex ) {
+            swipeUpElement( 700, 600);
+        }
+    }
+
+    public void swipeUpElement( final int y2, final int y1 ) {
+        try {
+            TouchAction touchAction = new TouchAction( driver );
+            touchAction.press( 0, y2 ).moveTo( 0, y1 ).release().perform();
+            Thread.sleep( 5000 );
+        }
+        catch ( NoSuchElementException ex ){
+            ex.printStackTrace();
+            System.out.println( "element not found -- inside swipeUpElement " );
+        } catch ( InterruptedException e ) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private boolean isElementPresentById( String id) {
+        try {
+//            if( driver.findElements(By.xpath("//android.widget.Button[@text='Add to Basket']")).size()>0)
+            if( driver.findElement(
+                    By.xpath("//android.view.View[contains(@resource-id,'add-to-wishlist-button-submit') and @text='ADD TO LIST']"))
+                    .isDisplayed())
+                return true;
+        } catch ( NoSuchElementException ex ) {
+            swipeUpElement( 700, 500);
             return false;
         }
+        return false;
     }
+
+    private boolean isElementPresentByText( String text ) {
+        try {
+            if( driver.findElements(By.xpath("//*[contains(@text,'"+text+"')]")).size()>0)
+                return true;
+        } catch ( NoSuchElementException ex ) {
+            swipeUpElement(700, 600);
+            return false;
+        }
+        return false;
+    }
+
 
     @AfterTest
     public void End() {
