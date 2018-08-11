4cf277f20c287996043cc7cdee2c9b140023f021
Diana Devasia <dianadev@thoughtworks.com>
2018-08-11 22:54:19 +0200

    Finding element by text by replaceing the special character after digit and space

--- src/test/java/AppiumElement.java
+++ src/test/java/AppiumElement.java
@@ -1,5 +1,8 @@
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
 
 import io.appium.java_client.android.AndroidDriver;
 
@@ -15,13 +18,46 @@ public class AppiumElement {
 
     public boolean isPresentByText( String text ) {
         try {
-            if( driver.findElements( By.xpath("//*[contains(@text,'"+text+"')]")).size()>0)
-                return true;
+            final List<WebElement> itemTitlesElement = driver.findElements( By.id( "item_title" ) );
+            int counter = 0;
+            for( WebElement itemTitleElement : itemTitlesElement){
+                counter++;
+                final String titleText = itemTitleElement.getText();
+                final String[] splittedTitleWords = titleText.split( " " );
+                String s = "";
+                for (String splittedWord: splittedTitleWords){
+                    s = s.concat( splittedWord );
+                }
+                if( s.equals( text ))
+                    return true;
+            }
+
+//            if( driver.findElements( By.xpath("//*[contains(@text,'"+text+"')]")).size()>0)
+//                return true;
         } catch ( NoSuchElementException ex ) {
             appiumActions.swipeUpElement( driver, 700, 600);
             return false;
         }
         return false;
+    }
+
+
+    public int getIndexByText( String text ) {
+        try {
+            final List<WebElement> itemTitlesElement = driver.findElements( By.id( "item_title" ) );
+            int counter = 0;
+            for( WebElement itemTitleElement : itemTitlesElement){
+                final String titleText = itemTitleElement.getText();
+                String s = titleText.replaceAll( " ", "" ).replaceAll("\u00A0","");
+                if( s.equals( text ))
+                    return counter;
+                counter++;
+            }
+            return -1;
+        } catch ( NoSuchElementException ex ) {
+            appiumActions.swipeUpElement( driver, 700, 600);
+            return -1;
+        }
     }
 
 
